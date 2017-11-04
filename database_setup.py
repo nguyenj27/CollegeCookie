a03d78be221251c3be688b75e150281b2d1c205c
Seho Lim <limseho657424@gmail.com>
2017-11-04 08:44:31 -0500

    ddd

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, ForeignKey, DateTime, func, Boolean, float
+from sqlalchemy import Column, String, ForeignKey, DateTime, func, Boolean, \
+	Float
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -11,6 +12,7 @@ class User(Base):
 	name = Column(String(250), nullable = False)
 	password = Column(String(250), nullable = False)
 	school_name = Column(String(50), nullable = False)
+
 
 class Time(Base):
 	_tablename_= 'Time'
