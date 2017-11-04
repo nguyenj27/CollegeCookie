cb46f462b2f39156c80707616433946bc7a58995
Seho Lim <limseho657424@gmail.com>
2017-11-04 09:01:15 -0500

    found errors.

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, String, ForeignKey, DateTime, func, Boolean, \
-	Float
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, func, \
+	Float, Boolean
 
 from sqlalchemy.orm import relationship
 
@@ -14,9 +14,9 @@ class User(Base):
 	name = Column(String(250), nullable = False)
 	password = Column(String(250), nullable = False)
 	school_name = Column(String(50), nullable = False)
-	breakfast = Column(float, nullable = True)
-	lunch = Column(float, nullable = True)
-	dinner = Column(float, nullable = True)
+	breakfast = Column(Float, nullable = True)
+	lunch = Column(Float, nullable = True)
+	dinner = Column(Float, nullable = True)
 
 # class Time(Base):
 #	_tablename_= 'Time'
@@ -24,5 +24,5 @@ class User(Base):
 #	user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
 #	user = relationship(User)
 
-engine = create_engine('sqLite:///main.db')
+engine = create_engine('sqlite:///main.db')
 Base.metadate.create_all(engine)
