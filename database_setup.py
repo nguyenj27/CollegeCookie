98a0443c45460c786a420ff95d910d6c58c578e5
eselenic <selenicaelona@gmail.com>
2017-11-04 08:29:07 -0500

    I created a main page using HTML and styled it with CSS

---
+++ database_setup.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey, DateTime, func, Boolean, float
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+	_tablename_ = 'user'
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+	password = Column(String(250), nullable = False)
+	school_name = Column(String(50), nullable = False)
+
+class Time(Base):
+	_tablename_= 'Time'
+	interval = Column(Integer, primary_key = True)
+	breakfast = Column(float, nullable = False)
