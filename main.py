e7b2a5610ed422d1fab4887e869fcb61426d4754
Seho Lim <limseho657424@gmail.com>
2017-11-04 00:43:34 -0500

    added simple server.

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def main():
+    return "don't eat lunch alone friend!"
+
+
+@app.route("/login")
+def login():
+    return "this is the login page."
+
+@app.route("/2")
+def two():
     return "Hello World!"
+
+
+@app.route("/3")
+def three():
+    return "Hello World!"
+
+
+@app.route("/4")
+def four():
+    return "Hello World!"
+
 
 
 
